908cfdac00b545523f92b6d85154dd82c9c882ad
Pirog <95555086+gorip1@users.noreply.github.com>
2024-11-24 23:22:14 +0000

    fix reference to labels and not codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,29 +3,31 @@ import pandas as pd
 from st_supabase_connection import SupabaseConnection, execute_query
 import altair as alt
 
-allowed_values = ["Nombre de boîtes délivrées", "Montant Remboursé", "Base de Remboursement"]
+allowed_values = ["Montant Remboursé", "Nombre de boîtes délivrées", "Base de Remboursement"]
 allowed_col_index = ["Tranche d’Age du Bénéficiaire","Sexe du Bénéficiaire", "Région de Résidence du Bénéficiaire", "Prescripteur", "Année", "Libellé ATC4", "Libellé ATC5", "Code CIP 13", "Libellé CIP 13"]
+
+### GET DB FUNCTIONS
 
 @st.cache_data(persist=True)
 def fetch_atc3():
     result = execute_query(
-        st_supabase_client.table("atc3_values").select("*"), ttl=0
+        st_supabase_client.table("atc3_code_and_labels").select("*"), ttl=0
     )
     return pd.DataFrame(result.data)
 
 @st.cache_data(persist=True)
 def fetch_atc5():
     result = execute_query(
-        st_supabase_client.table("l_atc5_values").select("*"), ttl=0
+        st_supabase_client.table("atc5_code_and_labels").select("*"), ttl=0
     )
     return pd.DataFrame(result.data)
 
 @st.cache_data(persist=True)
-def fetch_drug_atc3(l_atc5):
+def fetch_drug_atc3(atc5):
     result = execute_query(
-        st_supabase_client.table("test10")
-        .select("Libellé ATC3")
-        .eq("Libellé ATC5", l_atc5)
+        st_supabase_client.table("test11")
+        .select("Code ATC3", "Libellé ATC3")
+        .eq("Code ATC5", atc5)
         .limit(1)
     )
     return pd.DataFrame(result.data)
@@ -33,10 +35,13 @@ def fetch_drug_atc3(l_atc5):
 @st.cache_data(persist=True)
 def fetch_data(column, value):
     result = execute_query(
-        st_supabase_client.table("test10").select("*").ilike(column, f"%{value}%")
+        st_supabase_client.table("test11").select("*").ilike(column, f"%{value}%")
     )
     return pd.DataFrame(result.data)   
+
+
 # Function to create a dynamic pivot table
+
 def create_pivot_table(df):
     if df is not None:
         st.sidebar.header("☕️ Filtre")
@@ -50,13 +55,11 @@ def create_pivot_table(df):
         st.sidebar.header("🧨 Tableau croisé dynamique")
         # Select pivot fields
         index_cols = st.sidebar.multiselect("Lignes", options=allowed_col_index, default='Libellé ATC5', placeholder="Choisir une ligne...")
-        column_cols = st.sidebar.multiselect("Colonnes (sous-catégorie)", options=allowed_col_index, placeholder="Choisir une colonne...")
+        column_cols = st.sidebar.multiselect("Colonnes (sous-catégorie)", options=allowed_col_index, placeholder="Choisir une colonne...", default="Année")
         value_col = st.sidebar.selectbox("Valeurs", options=allowed_values, key="values")
-        agg_func_fr = st.sidebar.selectbox("Calcul par...", ["Moyenne","Somme", "Nombre"], key="aggregation")
+        agg_func_fr = st.sidebar.selectbox("Calcul par...", ["Somme"], key="aggregation")
         agg_func_mapping = {
-            "Moyenne": "mean",
             "Somme": "sum",
-            "Nombre": "count"
         }
 
         agg_func = agg_func_mapping.get(agg_func_fr, "mean") 
@@ -74,9 +77,11 @@ def create_pivot_table(df):
             st.write("### Tableau croisé dynamique")
             st.dataframe(pivot_table, use_container_width=True)
             return pivot_table, len(index_cols), len(column_cols), agg_func_fr, value_col, index_cols
-        else: return None, 0, 0
+        else: return None, 0, 0, None, 0, 0
+
 
 # Function to visualize pivot table
+
 def visualize_data(df, index_col_count, column_col_count , agg_func_fr, value_col, index_cols):
     if df is not None:
         st.write("### Figure")
@@ -114,7 +119,9 @@ def visualize_data(df, index_col_count, column_col_count , agg_func_fr, value_co
                               )
         else:
             st.markdown("_La représentation graphique est disponible avec 2 dimensions._\n\n_Selectionnez seulement une ligne et une colonne_")
-    
+    elif index_col_count == 0:
+        st.write("")
+        st.markdown("_Veuillez choisir au moins une ligne pour afficher le tableau croisé et un graphique_")
 # Main Streamlit App
 if __name__ == "__main__":
     st.title("💊 Easy Open Medic")
@@ -129,27 +136,52 @@ if __name__ == "__main__":
     # Medication Search
     st.subheader("🔍 Recherche par médicament (DCI) ou classe ATC3")
     
-    atc5_df = fetch_atc5()  # Assuming fetch_data function can be reused for ATC5
-    atc5_values = atc5_df['L_ATC5'].unique().tolist()
-    selected_atc5 = st.selectbox("Choisir un médicament", options=atc5_values, placeholder="Choisir un médicament (DCI)", index=None)
-    
-    if selected_atc5 == None:
+    #SELECT A DRUG ENTRY POINT
+    atc5_df = fetch_atc5()
+    atc5_display_values = atc5_df['L_ATC5'].tolist()
+    selected_display_value = st.selectbox(
+        "Choisir un médicament", 
+        options=atc5_display_values, 
+        placeholder="Choisir un médicament (DCI)",
+        index=None
+    )
+    selected_value_df = atc5_df.loc[atc5_df['L_ATC5'] == selected_display_value, 'ATC5']
+    if not selected_value_df.empty:
+        selected_atc5 = selected_value_df.iat[0]
+    else:
+        selected_atc5 = None
+
+    if selected_atc5 is None:
         atc3_df = fetch_atc3()
-        atc3_values = atc3_df['L_ATC3'].tolist()
+        atc3_display_values = atc3_df['L_ATC3'].tolist()
         st.write("Ou...")
-        selected_atc3 = st.selectbox("Choisir une classe ATC3 🤓", index=None ,options=atc3_values, key="selected_atc3", placeholder="selectionnez une classe ATC3...")
-    
+        selected_display_value_atc3 = st.selectbox(
+            "Choisir une classe ATC3 🤓", 
+            options=atc3_display_values,
+            key="selected_atc3", 
+            placeholder="Selectionnez une classe ATC3...",
+            index=None
+        )
+        selected_value_df_atc3 = atc3_df.loc[atc3_df['L_ATC3'] == selected_display_value_atc3, 'ATC3']
+        if not selected_value_df_atc3.empty:
+            selected_atc3 = selected_value_df_atc3.iat[0]
+            selected_atc3_label = selected_display_value_atc3
+        else:
+            selected_atc3 = None
+            selected_atc3_label = None
+
     if selected_atc5:
         drug_atc3_df = fetch_drug_atc3(selected_atc5)
         if not drug_atc3_df.empty:
-            selected_atc3_from_db = drug_atc3_df.iloc[0]['Libellé ATC3']
-            selected_atc3 = selected_atc3_from_db
+            selected_atc3_label = drug_atc3_df.iloc[0]['Libellé ATC3']
+            selected_atc3 = drug_atc3_df.iloc[0]['Code ATC3']
         else:
-            selected_atc3_from_db = None
+            selected_atc3_label = None
+            selected_atc3 = None
 
     if selected_atc3:
-        st.header(selected_atc3)
-        open_medic_table = fetch_data('Libellé ATC3', selected_atc3)
+        st.header(selected_atc3_label)
+        open_medic_table = fetch_data('Code ATC3', selected_atc3)
         
         st.dataframe(open_medic_table, height=200)
         if len(open_medic_table) == 200000:
